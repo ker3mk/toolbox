8a09ca2aa25cd6c893f61bbd7b5de34d8bcd961d
Kerem Kuzu <ker3m.kuzu@gmail.com>
2022-02-21 09:43:52 +0300

    feat: info static field for main

---
+++ klibrary/klibrary-utils/src/main/java/com/klibrary/trickycodes/HelloWithEmptyMain.java
@@ -0,0 +1,11 @@
+package com.klibrary.trickycodes;
+
+public class HelloWithEmptyMain {
+
+    static {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+    }
+}
